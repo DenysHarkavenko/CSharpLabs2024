ea5e949f7c1d990a515061dfa367655895879266
Denys Harkavenko <garkavenkodenispost@gmail.com>
2024-03-11 00:20:39 +0100

    Second task

--- CSharpLabs_2024/Pr_3/Modules/Book.cs
+++ CSharpLabs_2024/Pr_3/Modules/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pr_3.Modules
+﻿namespace Pr_3.Modules
 {
     internal class Book
     {
